6dee259233808eff47b69396a409921a27a5b52f
revalinda <revalindaph@gmail.com>
2025-11-19 12:14:19 +0700

    Percobaan 1

---
+++ Bioskop21.java
@@ -0,0 +1,31 @@
+public class Bioskop21 {
+    public static void main(String[] args) {
+        String[][] penonton = new String[4][2];
+
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+
+        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+        /*
+        Pertanyaan
+        1. Apakah pengisian elemen array harus dilakukan secara berurutan mulai dari
+           indeks ke-0? Jelaskan!
+           = tidak harus, bisa di isi dari mana saja tidak harus dari 0 karena
+           array hanya butuh indeks yang valid, bukan urutan pengisian.
+        2. Mengapa terdapat null pada daftar nama penonton? 
+           = karena array 2D di isi dengan indeks baris 4 dan kolom 2, namun input
+           hanya berhenti pada baris 3 kolom 0, dan jika di hitung hanya terdapat 3 kolom 1
+           pada coding, itu kurang pengisian lagi pada kolom 1 baris ke 3
+        3. Lengkapi daftar penonton pada langkah ke-4 sebagai berikut
+        */
+    }
+}
