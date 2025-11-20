34e144cc755a503ee6eaef53713835ea88cb18f7
revalinda <revalindaph@gmail.com>
2025-11-20 16:17:25 +0700

    Percobaan 4 Modif 1

--- SIAKAD21.java
+++ SIAKAD21.java
@@ -3,30 +3,37 @@ import java.util.Scanner;
 public class SIAKAD21 {
     public static void main(String[] args) {
         Scanner Linda = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
 
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i+1));
+        System.out.println("Masukkan jumlah siswa: ");
+        int jumlahSiswa = Linda.nextInt();
+
+        System.out.println("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = Linda.nextInt();
+
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i+1));
             double totalPersiswa = 0;
-            for (int j = 0; j < nilai.length; j++) {
+            for (int j = 0; j < jumlahMatkul; j++) {
                 System.out.println("Nilai mata kuliah " + (j+1) + ": ");
                 nilai[i][j] = Linda.nextInt();
                 totalPersiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata: " + totalPersiswa/3);
+            System.out.println("Nilai rata-rata mahasiswa ke- " + (i+1) + totalPersiswa/jumlahMatkul);
         }
 
         System.out.println("\n======================================");
         System.out.println("Rata-rataa Nilai setiap Mata Kuliah:");
 
-        for (int i = 0; i < nilai.length; i++) {
+        for (int j = 0; j < jumlahMatkul; j++) {
             double totalPerMatkul = 0;
-            for (int j = 0; j < nilai.length; j++) {
+            for (int i = 0; i < jumlahSiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata Kuliah " + ": " + totalPerMatkul/4);
+            System.out.println("Mata Kuliah " + (j+1) + ": " + totalPerMatkul/jumlahSiswa);
         }
-        
+
         Linda.close();
     }
 }
