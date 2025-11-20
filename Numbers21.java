8f416160069d75a0c61d83d4a7ff6ce80fd5e938
revalinda <revalindaph@gmail.com>
2025-11-20 15:42:28 +0700

    Percobaan 3

---
+++ Numbers21.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Numbers21 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+    }
+}
+
+/*
+1. Tambahkan kode program sebagai berikut.
+2. Apa fungsi dari Arrays.toString()? 
+3. Apa nilai default untuk elemen pada array dengan tipe data int? 
+4. Tambahkan kode program berikut 
+5. Array myNumbers memiliki length berbeda untuk setiap barisnya. Apakah panjang 
+array dapat dimodifikasi setelah diinstansiasi?
+*/
