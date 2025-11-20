d3db1bfe226dfe686a759f017d85da46a2320417
revalinda <revalindaph@gmail.com>
2025-11-20 15:18:21 +0700

    Percobaan 2 Modif No.2

--- BioskopWithScanner.java
+++ BioskopWithScanner.java
@@ -8,27 +8,55 @@ public class BioskopWithScanner {
         int baris, kolom;
 
         while (true) {
-            System.out.print("Masukkan nama : ");
-            String nama = Linda.nextLine();
-            System.out.print("Masukkan baris : ");
-            baris = Linda.nextInt();
-            System.out.print("Masukkan kolom : ");
-            kolom = Linda.nextInt();
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            int menu = Linda.nextInt();
             Linda.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
-
-            System.out.print("Input penonton lainnya? (y/n): ");
-            String next = Linda.nextLine();
-
-            if (next.equalsIgnoreCase("n")) {
-                break;
+        if (menu == 1) {
+            while (true) {  
+                System.out.print("Masukkan nama : ");
+                String nama = Linda.nextLine();
+                System.out.print("Masukkan baris : ");
+                baris = Linda.nextInt();
+                System.out.print("Masukkan kolom : ");
+                kolom = Linda.nextInt();
+                Linda.nextLine();
+                
+                penonton[baris-1][kolom-1] = nama;
+                
+                System.out.print("Input penonton lainnya? (y/n): ");
+                String next = Linda.nextLine();
+                
+                if (next.equalsIgnoreCase("n")) {
+                    break;
+                }
             }
 
         }
 
-        Linda.close();
+        else if (menu == 2) {
+            for (int i = 0; i < penonton.length; i++) {
+                for (int j = 0; j < penonton[i].length; j++) {
+                    if (penonton[i][j] == null) {
+                        System.out.print("null\t");
+                     } else {
+                        System.out.print(penonton[i][j] + "\t");
+                    }
+                }
+            System.out.println();
+            }
+        } else if (menu == 3) {
+            System.out.println("Keluar dari program...");
+            break;
+        } else {
+            System.out.println("Menu tidak tersedia!");
+        }
+    }
 
+        Linda.close();
     }
 }
 
@@ -40,6 +68,7 @@ public class BioskopWithScanner {
    Menu 1: Input data penonton 
    Menu 2: Tampilkan daftar penonton 
    Menu 3: Exit 
+   Done
 3. Modifikasi kode program untuk menghandle apabila nomor baris/kolom kursi yang 
    tidak tersedia 
 4. Pada menu 1, modifikasi kode program untuk memberikan warning apabila kursi yang 
